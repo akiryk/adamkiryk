29d5a33e21ffaeaf855c916443d7536697ff7696
Adam Kiryk <adamkiryk@gmail.com>
2017-07-19 19:24:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Adam Kiryk Website
---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+const
+  // modules
+  gulp = require('gulp'),
+  newer = require('gulp-newer'),
+  imagemin = require('gulp-imagemin'),
+  htmlclean = require('gulp-htmlclean'),
+  concat = require('gulp-concat'),
+  deporder = require('gulp-deporder'),
+  stripdebug = require('gulp-strip-debug'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-sass'),
+  postcss = require('gulp-postcss'),
+  assets = require('postcss-assets'),
+  autoprefixer = require('autoprefixer'),
+  mqpacker = require('css-mqpacker'),
+  cssnano = require('cssnano'),
+  babel = require('gulp-babel'),
+
+// development mode?
+  devBuild = true,
+
+folder = {
+  src: 'src/',
+  build: 'docs/'
+}
+
+// image processing
+gulp.task('images', function() {
+  var out = folder.build + 'images/';
+  return gulp.src(folder.src + 'images/**/*')
+    .pipe(newer(out))
+    .pipe(imagemin({ optimizationLevel: 9 }))
+    .pipe(gulp.dest(out));
+});
+
+// HTML processing
+gulp.task('html', ['images'], function() {
+  var
+    out = folder.build,
+    page = gulp.src(folder.src + 'index.html')
+      .pipe(newer(out));
+
+  // minify production code
+  if (!devBuild) {
+    page = page.pipe(htmlclean());
+  }
+
+  return page.pipe(gulp.dest(out));
+});
+
+// JavaScript processing
+gulp.task('js', function() {
+
+  var jsbuild = gulp.src(folder.src + 'js/**/*')
+    .pipe(deporder())
+    .pipe(concat('main.js'));
+
+  if (!devBuild) {
+    jsbuild = jsbuild
+      .pipe(stripdebug())
+      .pipe(uglify());
+  }
+
+  return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
+
+});
+
+// CSS processing
+gulp.task('css', ['images'], function() {
+
+  var postCssOpts = [
+  assets({ loadPaths: ['images/'] }),
+  autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+  mqpacker
+  ];
+
+  if (!devBuild) {
+    postCssOpts.push(cssnano);
+  }
+
+  return gulp.src(folder.src + 'scss/main.scss')
+    .pipe(sass({
+      outputStyle: 'nested',
+      imagePath: 'images/',
+      precision: 3,
+      errLogToConsole: true
+    }))
+    .pipe(postcss(postCssOpts))
+    .pipe(gulp.dest(folder.build + 'css/'));
+
+});
+
+gulp.task('babel', function() {
+ return gulp.src('docs/js/main.js')
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(gulp.dest('docs/js/'));
+})
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
